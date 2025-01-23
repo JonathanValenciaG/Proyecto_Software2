08b0226c27880dae69b9ab914732d249220fc833
Daniel Felipe Carreño <127898018+Paipo27@users.noreply.github.com>
2025-01-23 16:21:52 -0500

    Add files via upload

---
+++ FuncionesSW2.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+
+def mostrar_inicio():
+    st.title("Bienvenido al Taller Mecánico")
+    st.write("Aquí puedes gestionar tus citas y tus registros para tu servico de Mecanica.")
+
+# Función para mostrar el formulario de registro
+def formulario_registro():
+    st.title("Formulario de Registro")
+    with st.form("registro_usuario"):
+
+        nombre = st.text_input('Nombre Completo')
+        telefono = st.text_input('Teléfono')
+        email = st.text_input('Correo Electrónico')
+        
+        # Selectbox para elegir el tipo de vehículo
+        Vehiculo = st.selectbox('Tipo de Vehículo', ['Carro', 'Moto', "Motocarro", "Buseta", "Tractomula", "Cuatrimoto"])
+        
+        DetallesVehiculo = st.text_input('Marca y Modelo del Vehículo')
+        placa = st.text_input('Número de Placa')
+        problema = st.text_area('Descripción del Problema')
+        
+        # Botón para registrar
+        submit = st.form_submit_button('Registrar')
+        if submit:
+            if nombre and telefono and email and Vehiculo and DetallesVehiculo and placa and problema:
+                # Guardar los datos en el estado de la sesión
+                st.session_state['user_data'].append({
+                    'Nombre': nombre,
+                    'Teléfono': telefono,
+                    'Correo': email,
+                    "Vehiculo": Vehiculo,
+                    'Detalles': DetallesVehiculo,
+                    'Placa': placa,
+                    'Problema': problema
+                })
+                st.success(f'Registro completado para {nombre}')
+            else:
+                st.error('Por favor, complete todos los campos.')
+
+# Función para mostrar los registros de clientes
+def mostrar_registros():
+    st.title("Registros de Clientes")
+    if st.session_state['user_data']:
+        registros_df = pd.DataFrame(st.session_state['user_data'])
+        st.dataframe(registros_df)
+    else:
+        st.write('No hay registros disponibles aún.')
+
+# Función para mostrar el mapa de Manizales
+def mapa_manizales():
+    manizales_center = {
+        'latitude': 5.0689,
+        'longitude': -75.5174
+    }
+
+    # Configuración del mapa
+    view_state = pdk.ViewState(
+        latitude=manizales_center['latitude'],
+        longitude=manizales_center['longitude'],
+        zoom=12  # Ajusta el nivel de zoom para mostrar la ciudad
+    )
+
+    # Crear el mapa sin puntos
+    mapa = pdk.Deck(map_style='mapbox://styles/mapbox/streets-v11', initial_view_state=view_state)
+
+    # Mostrar el mapa
+    st.title("Mapa de Manizales, Colombia")
+    st.pydeck_chart(mapa)
---
+++ Ingenieria2.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from FuncionesSW2 import *
+
+
+# Inicializa la lista para guardar registros si no existe
+if 'user_data' not in st.session_state:
+    st.session_state['user_data'] = []
+
+page = st.sidebar.radio("Opciones de Registro", ("Inicio", "Formulario de Registro", "Registros", "Mapa de Manizales"))
+
+# Ejecuta la función correspondiente según la opción seleccionada
+if page == "Inicio":
+    mostrar_inicio()
+
+elif page == "Formulario de Registro":
+    formulario_registro()
+
+elif page == "Registros":
+    mostrar_registros()
+
+elif page == "Mapa de Manizales":
+    mapa_manizales()
