bcf58bd0b0014de19c38d5c8b742c768778efa63
Oscar Alejandro <oramirezr@unal.edu.co>
2025-02-01 21:22:25 -0500

    Commit de prueba

--- FuncionesSW2.py
+++ FuncionesSW2.py
@@ -1,29 +1,16 @@
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
-from BasedeDatos1 import FirebaseDB
-from firebase_admin import db
-import os
-
-# Proporciona la ruta completa al archivo BaseDatos.json
-path = os.path.join(os.path.dirname(__file__), 'BaseDatos.json')
-url = "https://proyectois2-cabb0-default-rtdb.firebaseio.com/"
-
-# Verifica si el archivo existe
-if not os.path.exists(path):
-    st.error(f'El archivo {path} no existe. Por favor, verifica la ruta.')
-
-# Inicializa la base de datos
-firebase_db = FirebaseDB(path, url)
 
 def mostrar_inicio():
     st.title("Bienvenido al Taller Mecánico")
     st.write("Aquí puedes gestionar tus citas y tus registros para tu servico de Mecanica.")
 
-# Función para mostrar el formulario de registro de usuarios
+# Función para mostrar el formulario de registro
 def formulario_registro():
-    st.title("Formulario de Registro ")
+    st.title("Formulario de Registro")
     with st.form("registro_usuario"):
+
         nombre = st.text_input('Nombre Completo')
         telefono = st.text_input('Teléfono')
         email = st.text_input('Correo Electrónico')
@@ -39,35 +26,28 @@ def formulario_registro():
         submit = st.form_submit_button('Registrar')
         if submit:
             if nombre and telefono and email and Vehiculo and DetallesVehiculo and placa and problema:
-                # Guardar los datos en Firebase Realtime Database
-                data = {
-                    "nombre": nombre,
-                    "telefono": telefono,
-                    "email": email,
+                # Guardar los datos en el estado de la sesión
+                st.session_state['user_data'].append({
+                    'Nombre': nombre,
+                    'Teléfono': telefono,
+                    'Correo': email,
                     "Vehiculo": Vehiculo,
-                    "DetallesVehiculo": DetallesVehiculo,
-                    "placa": placa,
-                    "problema": problema
-                }
-                try:
-                    firebase_db.write_record(f'registros/{nombre}', data)
-                    st.success('Registro exitoso')
-                except Exception as e:
-                    st.error(f'Error en el registro: {e}')
+                    'Detalles': DetallesVehiculo,
+                    'Placa': placa,
+                    'Problema': problema
+                })
+                st.success(f'Registro completado para {nombre}')
+            else:
+                st.error('Por favor, complete todos los campos.')
 
 # Función para mostrar los registros de clientes
 def mostrar_registros():
     st.title("Registros de Clientes")
-    try:
-        registros = firebase_db.read_record('registros')
-        if registros:
-            # Convertir los registros en un DataFrame de Pandas para mostrarlos en Streamlit
-            registros_df = pd.DataFrame.from_dict(registros, orient='index')
-            st.dataframe(registros_df)
-        else:
-            st.write('No hay registros disponibles aún.')
-    except Exception as e:
-        st.error(f'Error al obtener los registros: {e}')
+    if st.session_state['user_data']:
+        registros_df = pd.DataFrame(st.session_state['user_data'])
+        st.dataframe(registros_df)
+    else:
+        st.write('No hay registros disponibles aún.')
 
 # Función para mostrar el mapa de Manizales
 def mapa_manizales():
